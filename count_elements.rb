250eb775149d6e02ce27492eeed00136839c4d1e
zonika <zchodosh@conncoll.edu>
2015-08-03 15:13:31 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  new_hash={}
+  array.each do |animal|
+    new_hash[animal]=0
+  end
+  array.each do |animal|
+    new_hash[animal]+=1
+  end
+  new_hash
 end
  
